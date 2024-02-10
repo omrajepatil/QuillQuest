8659a7229cb3845bd4e319f3aa2e59343ee542fd
om <patilomraje590@gmail.com>
2024-02-11 00:33:25 +0530

    commit

--- src/server/routes/blogRoutes.js
+++ src/server/routes/blogRoutes.js
@@ -8,8 +8,8 @@ router.get("/", (req, res)=>{
 
 router.get("/blog/tech" , async(req, res)=>{
     try{
-        const posts = await User.find({ type: '1' });
-    res.render("blogs.ejs", {content:"Technical Blogs",posts:posts})
+        const posts = await User.find({ type: '1' }).maxTimeMS(30000);;
+    res.render("blogs.ejs", {content:"Technical Blogs",posts})
     }catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
@@ -17,8 +17,8 @@ router.get("/blog/tech" , async(req, res)=>{
 });
 router.get("/blog/chronicles" , async(req, res)=>{
     try{
-        const posts = await User.find({ type: '2' });
-    res.render("blogs.ejs", {content:"History Blogs",posts:posts})
+        const posts = await User.find({ type: '2' }).maxTimeMS(30000);;
+    res.render("blogs.ejs", {content:"History Blogs",posts})
     }catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
@@ -27,8 +27,8 @@ router.get("/blog/chronicles" , async(req, res)=>{
 })
 router.get("/blog/entertain" , async(req, res)=>{
     try{
-        const posts = await User.find({ type: '3' });
-    res.render("blogs.ejs", {content:"Entertainment Blogs",posts:posts})
+        const posts = await User.find({ type: '3' }).maxTimeMS(30000);;
+    res.render("blogs.ejs", {content:"Entertainment Blogs",posts})
     }catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
@@ -37,8 +37,8 @@ router.get("/blog/entertain" , async(req, res)=>{
 })
 router.get("/blog/headlines" , async(req, res)=>{
     try{
-        const posts = await User.find({ type: '4' });
-    res.render("blogs.ejs", {content:"Current Affair Blogs",posts:posts})
+        const posts = await User.find({ type: '4' }).maxTimeMS(30000);;
+    res.render("blogs.ejs", {content:"Current Affair Blogs",posts})
     }catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
@@ -74,24 +74,57 @@ catch(error){
 })
 
 
-router.delete('/blog/:id', async (req, res) => {
+//   delete route
+
+router.delete('/blog/:_id', async (req, res) => {
+    const _id = req.params._id;
     try {
-        const postId = req.params.id;
-        const deletedPost = await BlogPost.findByIdAndDelete(postId);
-        
+        const deletedPost = await User.findByIdAndDelete(_id);
+        res.redirect('/');
         if (!deletedPost) {
             return res.status(404).send('Blog post not found');
         }
-        
-        res.status(200).send('Blog post deleted successfully');
-    } catch (error) {
-        console.error('Error deleting blog post:', error);
+       
+    } catch (err) {
+        console.error(err);
         res.status(500).send('Internal Server Error');
     }
 });
 
 
 
+//  edit route
 
+router.get('/blog/edit/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+        if (!mongoose.Types.ObjectId.isValid(postId)) {
+            return res.status(400).send('Invalid post ID');
+        }
+        const post = await User.findById(postId);
+        if (!post) {
+            return res.status(404).send('Post not found');
+        }
+        res.render('blog_form.ejs', { post });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/blog/edit/:id', async (req, res) => {
+    try {
+        const postId = req.params.id;
+        const updatedContent = req.body.content;
+        const updatedPost = await User.findByIdAndUpdate(postId, { content: updatedContent }, { new: true });
+        if (!updatedPost) {
+            return res.status(404).send('Post not found');
+        }
+        res.redirect(`/blog/${postId}`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 
 module.exports = router;
