e7dc0ae92ce6f8738ed688ba9239e811ab92e712
om <patilomraje590@gmail.com>
2024-03-15 18:09:37 +0530

    l

--- src/server/routes/blogRoutes.js
+++ src/server/routes/blogRoutes.js
@@ -6,16 +6,16 @@ router.get("/", (req, res)=>{
 })
 
 router.get("/blog/tech" , async(req, res)=>{
-    res.render("blogs.ejs", {content:"Technical Blogs"})
+    res.render("blogs1.ejs", {content:"Technical Blogs"})
 })
 router.get("/blog/chronicles" , async(req, res)=>{
-    res.render("blogs.ejs", {content:"History Blogs"})
+    res.render("blogs1.ejs", {content:"History Blogs"})
 })
 router.get("/blog/entertain" , async(req, res)=>{
-    res.render("blogs.ejs", {content:"Entertainment Blogs"})
+    res.render("blogs1.ejs", {content:"Entertainment Blogs"})
 })
 router.get("/blog/headlines" , async(req, res)=>{
-    res.render("blogs.ejs", {content:"Current Affair Blogs"})
+    res.render("blogs1.ejs", {content:"Current Affair Blogs"})
 })
 router.get("/blog/single" , async(req, res)=>{
     res.render("single.ejs", {content:"Current Affair Blogs"})
